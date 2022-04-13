5082adb8d5fffa25b79927d8a4cd1a882a0eec90
Arseniy Yurchenko <yurchenkoa@cp3238-lx>
2022-04-13 11:19:51 +0300

    Refactoring

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.get('/', (req,res) => {
 })
 
 const host = '0.0.0.0';
-// const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.CONNECTION_URL)
